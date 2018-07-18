1d22327c6572bc2253c01420382718c34199350d
Andrew Och <andrewoch95@gmail.com>
2018-07-18 16:40:20 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
 # code here!
+
+class School
+
+attr_accessor
+attr_reader :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if @roster.keys.include?(grade)
+      @roster[grade].push(name)
+    else
+      @roster[grade] = [name]
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    new_hash = {}
+    @roster.each do |key, array|
+      new_hash[key] = array.sort!
+    end
+  end
+
+end
