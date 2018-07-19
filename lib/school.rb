46b18d93582d2025b646bf77474f1dae87ba75b9
github username <github email address>
2018-07-19 12:34:06 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+class School
+attr_accessor :school
+attr_reader :roster
+
+def initialize(school)
+  @school = school
+  @roster = {}
+end
+
+def add_student(name, grade)
+  if @roster.key?(grade)
+    @roster[grade] << name
+  else
+    @roster[grade] = [name]
+end
+end
+
+def grade(grade)
+@roster[grade]
+end
+
+
+def sort
+  Hash[@roster.sort.map { |a,b| [a, b.sort] }]
+
+end
+
+
+end
