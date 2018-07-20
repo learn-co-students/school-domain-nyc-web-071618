328d9c33a0123e068e2210d53dcd24394220bf5e
Your Name <you@example.com>
2018-07-20 17:17:04 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+class School
+  attr_accessor :roster
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  def add_student(name, grade)
+    if @roster.include?(grade)
+        @roster[grade] << name
+    else
+      @roster[grade] = [name]
+  end
+end
+
+  def grade(num)
+    @roster[num]
+ end
+
+def sort
+  sorted = {}
+  @roster.collect do |grade, student|
+    sorted[grade] = student.sort
+  end
+ sorted
+ end
+end
